2d29bc41fe2c700f2c8af110766dbf612e986400
sgw-ryota <ryota-segawe@sekawamukufutoshinoMacBook-Pro.local>
2022-05-17 23:25:19 +0900

    パスワード再設定用メール送信画面追加

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import { useRoutes } from 'react-router-dom';
 
 import { Top } from '../features/top';
 import { RadioStations } from '../features/radio_station';
-import { Register } from '../features/auth';
+import { Register, ForgotPassword } from '../features/auth';
 
 export const AppRoutes = () => {
     const commonRoutes = [{ path: '/', element: <Top /> }];
@@ -11,7 +11,10 @@ export const AppRoutes = () => {
     const radioStations = [{ path: '/radio_stations', element: <RadioStations /> }];
 
     // 認証関連
-    const auth = [{ path: '/register', element: <Register /> }]
+    const auth = [
+        { path: '/register', element: <Register /> },
+        { path: 'forgot_password', element: <ForgotPassword /> }
+    ]
 
     const element = useRoutes([...commonRoutes, ...radioStations, ...auth]);
 

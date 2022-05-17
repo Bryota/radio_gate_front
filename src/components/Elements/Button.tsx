cd1f6bb13bef25ac9c1c3e3d7158a2bf854734b3
sgw-ryota <ryota-segawe@sekawamukufutoshinoMacBook-Pro.local>
2022-05-17 23:08:08 +0900

    Buttonのスタイル修正

--- src/components/Elements/Button.tsx
+++ src/components/Elements/Button.tsx
@@ -7,9 +7,24 @@ type ButtonType = {
 }
 
 export const Button = ({ text, type, click_action }: ButtonType): JSX.Element => {
-    return (
-        <div className='text-center'>
-            <button className='element-btn element-btn-post' onClick={click_action}>{text}</button>
-        </div>
-    )
+    if (type = 'post') {
+        return (
+            <div className='text-center'>
+                <button className='element-btn element-btn-post' onClick={click_action}>{text}</button>
+            </div>
+        )
+    } else if (type = 'get') {
+        return (
+            <div className='text-center'>
+                <button className='element-btn element-btn-get' onClick={click_action}>{text}</button>
+            </div>
+        )
+    } else {
+        return (
+            <div className='text-center'>
+                <button className='element-btn element-btn-delete' onClick={click_action}>{text}</button>
+            </div>
+        )
+    }
+
 }

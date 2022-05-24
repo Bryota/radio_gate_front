12273c21011ab2803f272230b5ba4541168ab1bb
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-05-24 22:36:59 +0900

    管理者機能リクエスト作成画面追加

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -2,3 +2,4 @@ export * from './Input';
 export * from './CheckBox';
 export * from './Textarea';
 export * from './admin/Input';
+export * from './admin/Textarea';
--- src/features/admin/request_function/index.ts
+++ src/features/admin/request_function/index.ts
@@ -1,1 +1,2 @@
 export * from './components/RequestFunctions';
+export * from './components/CreateRequestFunction';

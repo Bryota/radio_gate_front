a4e9ba5a8c5d881f2dcefbaf928e38118f12ad6c
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-05-30 23:33:04 +0900

    ログイン処理追加

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -1,13 +1,36 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from '../../../settings/Axios';
+
 import { MainLayout, InnerBox } from '../../../components/Layout';
 import { Pagehead } from '../../../components/Pagehead';
 import { Input } from '../../../components/Form/Input';
 import { Button } from '../../../components/Elements/Button';
 
 export const Login = () => {
+    const [email, setEmail] = useState<string>();
+    const [password, setPassword] = useState<string>();
+    const navigation = useNavigate()
 
     const click_handler = () => {
-        return '';
+        axios.get(`${process.env.REACT_APP_RADIO_GATE_API_URL}/sanctum/csrf-cookie`)
+            .then(() => {
+                axios.post(`${process.env.REACT_APP_RADIO_GATE_API_URL}/api/login`, {
+                    email,
+                    password
+                }).then(res => {
+                    // TODO: エラー時の処理追加
+                    if (res.status == 200) {
+                        return (
+                            navigation('/message_post')
+                        )
+                    } else {
+                        console.log(res.data.message);
+                    }
+                })
+            })
     }
+
     return (
         <>
             <MainLayout>
@@ -21,11 +44,13 @@ export const Login = () => {
                         text='メールアドレス'
                         type="email"
                         is_first_item={true}
+                        change_action={e => setEmail(e.target.value)}
                     />
                     <Input
                         key='password'
                         text='パスワード'
                         type="password"
+                        change_action={e => setPassword(e.target.value)}
                     />
                 </InnerBox>
                 <Button

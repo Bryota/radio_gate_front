4d33f72ea1c19955274130c80e270e3d5ccc37b0
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-06-06 23:57:04 +0900

    機能リクエスト一覧API繋ぎ込み

--- src/features/request_function/components/RequesetFunctions.tsx
+++ src/features/request_function/components/RequesetFunctions.tsx
@@ -1,3 +1,7 @@
+import axios from '../../../settings/Axios';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
 import { MainLayout } from '../../../components/Layout';
 import { Pagehead } from '../../../components/Pagehead';
 import { Button } from '../../../components/Elements/Button';
@@ -6,10 +10,29 @@ import { RequestFunctionList } from './RequestFunctionList';
 import '../../../assets/css/elements/radio.css';
 import '../../../assets/css/components/pagination.css';
 
+type RequestFunctionsType = {
+    id: number
+    name: string
+    detail: string
+    point: number
+}
+
 export const RequestFunctions = () => {
-    const click_handler = () => {
-        return '';
-    }
+    const [requestFunctions, setRequestFunctions] = useState<RequestFunctionsType[]>([]);
+    const navigation = useNavigate();
+
+    useEffect(() => {
+        const fetchRequestFunctions = async () => {
+            try {
+                const RequestFunctionResponse = await axios.get(`${process.env.REACT_APP_RADIO_GATE_API_URL}/api/request_functions`);
+                setRequestFunctions(RequestFunctionResponse.data.request_functions);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchRequestFunctions();
+    }, []);
+
     return (
         <>
             <MainLayout>
@@ -21,29 +44,23 @@ export const RequestFunctions = () => {
                     text='機能リクエストを追加'
                     type='get'
                     line_left={true}
-                    click_action={click_handler}
+                    click_action={() => {
+                        navigation('/request_function/create')
+                    }}
                 />
                 <div>
-                    <RequestFunctionList
-                        name="住所を複数登録できる"
-                        point={15}
-                    />
-                    <RequestFunctionList
-                        name="住所を複数登録できる"
-                        point={15}
-                    />
-                    <RequestFunctionList
-                        name="住所を複数登録できる"
-                        point={15}
-                    />
-                    <RequestFunctionList
-                        name="住所を複数登録できる"
-                        point={15}
-                    />
-                    <RequestFunctionList
-                        name="住所を複数登録できる"
-                        point={15}
-                    />
+                    {
+                        requestFunctions.map(requestFunction => {
+                            return (
+                                <RequestFunctionList
+                                    key={requestFunction.id}
+                                    id={requestFunction.id}
+                                    name={requestFunction.name}
+                                    point={requestFunction.point}
+                                />
+                            )
+                        })
+                    }
                 </div>
                 <Pagination />
             </MainLayout>
--- src/features/request_function/components/RequestFunctionList.tsx
+++ src/features/request_function/components/RequestFunctionList.tsx
@@ -1,20 +1,21 @@
 import '../../../assets/css/elements/radio.css';
 
 type RequestFunctionType = {
+    id: number
     name: string
     point: number
 }
 
-export const RequestFunctionList = ({ name, point }: RequestFunctionType): JSX.Element => {
+export const RequestFunctionList = ({ id, name, point }: RequestFunctionType): JSX.Element => {
     return (
         <>
             <div className='row align-items-center mt-4 p-2rem list-item'>
                 <p className='col-5 font-25'>{name}</p>
                 <p className='col-2 h4'>{point}Pt</p>
-                <a href="#" className='col-2 btn list-btn'>
+                <a href={`/request_function/${id}/vote`} className='col-2 btn list-btn'>
                     投票する
                 </a>
-                <a href="#" className='col-2 offset-1 btn bg-main list-btn'>
+                <a href={`/request_function/${id}`} className='col-2 offset-1 btn bg-main list-btn'>
                     詳細
                 </a>
             </div>

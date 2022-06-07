6e4a0c7857ef3fa6231040aa4d6ddf55a247dd05
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-06-07 22:15:27 +0900

    axiosのインポート変更

--- src/features/radio_program/components/RadioProgram.tsx
+++ src/features/radio_program/components/RadioProgram.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from '../../../settings/Axios';
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 
--- src/features/radio_station/components/RadioStations.tsx
+++ src/features/radio_station/components/RadioStations.tsx
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from '../../../settings/Axios';
 import { useState, useEffect } from 'react';
 
 import { MainLayout } from '../../../components/Layout';

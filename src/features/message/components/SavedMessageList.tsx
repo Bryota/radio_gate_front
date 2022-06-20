b0663e102fe2b16e65ddf7986d54c8acbae739fd
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-06-20 12:44:52 +0900

    一時保存画面作成

---
+++ src/features/message/components/SavedMessageList.tsx
@@ -0,0 +1,24 @@
+type MessageListType = {
+    id: number
+    radio_program?: string
+    corner?: string
+}
+
+export const SavedMessageList = ({ id, radio_program, corner }: MessageListType): JSX.Element => {
+    return (
+        <>
+            <div className='row align-items-center mt-4 p-2rem list-item'>
+                <div className="col-7 row font-20">
+                    <p>{radio_program}</p>
+                    <p>{corner}</p>
+                </div>
+                <a href={`/message_post?saved_message=${id}`} className='col-2 text-center p-2 mx-4 list-btn bg-accent'>
+                    コーナーを送る
+                </a>
+                <a href={`/saved_message/${id}`} className='col-2 text-center p-2 list-btn bg-main'>
+                    詳細
+                </a>
+            </div>
+        </>
+    )
+}
---
+++ src/features/message/components/SavedMessages.tsx
@@ -0,0 +1,110 @@
+import axios from '../../../settings/Axios';
+import { useNavigate } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+
+import { MainLayout } from '../../../components/Layout';
+import { Pagehead } from '../../../components/Pagehead';
+import { Pagination } from '../../../components/Pagination';
+import { SavedMessageList } from './SavedMessageList';
+import { isAuthorized } from '../../../modules/auth/isAuthorized';
+import '../../../assets/css/elements/radio.css';
+import '../../../assets/css/components/pagination.css';
+
+type SavedMessageType = {
+    id: number
+    radio_program_id: string
+    program_corner_id: string
+    listener_my_program_id: string
+    my_program_corner_id: string
+    subject?: string
+    content: string
+    radio_name?: string
+    created_at: string
+    updated_at: string
+    listener_my_program?: {
+        name?: string
+    }
+    my_program_corner?: {
+        name?: string
+    }
+    radio_program?: {
+        name?: string
+    }
+    program_corner?: {
+        name?: string
+    }
+}
+
+export const SavedMessages = () => {
+    const [savedMessages, setSavedMessages] = useState<SavedMessageType[]>([]);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const navigation = useNavigate();
+
+    useEffect(() => {
+        authorized();
+        const fetchMessages = async () => {
+            try {
+                const SavedMessagesResponse = await axios.get(`${process.env.REACT_APP_RADIO_GATE_API_URL}/api/saved_messages?page=${currentPage}`);
+                setSavedMessages(SavedMessagesResponse.data.listener_messages.data);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        fetchMessages();
+    }, []);
+
+    const authorized = async () => {
+        let authorized = await isAuthorized();
+        if (!authorized) {
+            navigation('/login');
+        }
+    }
+
+    const prevPagination = () => {
+        setCurrentPage((pre_current_page) => pre_current_page - 1);
+    }
+
+    const nextPagination = () => {
+        setCurrentPage((pre_current_page) => pre_current_page + 1);
+    }
+    console.log(savedMessages)
+
+    return (
+        <>
+            <MainLayout>
+                <Pagehead
+                    title="SavedMessages"
+                    subtitle='一時保存一覧'
+                />
+                <div>
+                    {
+                        savedMessages.map(message => {
+                            if (message.radio_program) {
+                                return (
+                                    <SavedMessageList
+                                        id={message.id}
+                                        radio_program={message.radio_program?.name}
+                                        corner={message.program_corner ? message.program_corner?.name : message.subject}
+                                    />
+                                )
+                            } else {
+                                return (
+                                    <SavedMessageList
+                                        id={message.id}
+                                        radio_program={message.listener_my_program?.name}
+                                        corner={message.my_program_corner ? message.my_program_corner?.name : message.subject}
+                                    />
+                                )
+                            }
+                        })
+                    }
+                </div>
+                <Pagination
+                    currentPage={currentPage}
+                    prev_action={prevPagination}
+                    next_action={nextPagination}
+                />
+            </MainLayout>
+        </>
+    )
+}

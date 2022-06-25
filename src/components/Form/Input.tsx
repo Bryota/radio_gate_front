1d2af7cc0fbacf9067eb9a95ff2f15d7553e761f
sgw-ryota <ryota-segawe@sekawamukufutoshinoMacBook-Pro.local>
2022-06-25 22:29:19 +0900

    住所の自動入力機能追加

--- src/components/Form/Input.tsx
+++ src/components/Form/Input.tsx
@@ -13,10 +13,11 @@ type FormInputType = {
     is_post_code?: boolean
     is_first_item?: boolean
     validationMessages?: Array<validatedArrayType>
+    searchAddressByPostCode?: (postCode: string) => void
     change_action?: (event: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-export const Input = ({ key, value = '', text, type = 'text', is_post_code = false, is_first_item = false, validationMessages = [], change_action = () => { } }: FormInputType): JSX.Element => {
+export const Input = ({ key, value = '', text, type = 'text', is_post_code = false, is_first_item = false, validationMessages = [], searchAddressByPostCode = () => { }, change_action = () => { } }: FormInputType): JSX.Element => {
     return (
         <div className={is_first_item ? 'row mt-0 form-input_item' : 'row form-input_item'}>
             <div className='col-4'>
@@ -24,7 +25,7 @@ export const Input = ({ key, value = '', text, type = 'text', is_post_code = fal
             </div>
             <div className='col-8 position-relative'>
                 <input type={type} id={key} className='position-absolute underline-green' defaultValue={value} onChange={e => change_action(e)} />
-                {is_post_code && <button className='position-absolute'>自動入力</button>}
+                {is_post_code && <button className='position-absolute' onClick={() => searchAddressByPostCode(value)}>自動入力</button>}
             </div>
             {
                 validationMessages.map(validationMessage => {

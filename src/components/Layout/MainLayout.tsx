b6e51779ee9554bc48fec71284e1686ff9e05af5
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-07-04 21:48:58 +0900

    ナビバー追加

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Navbar } from './Navbar';
 import "normalize.css";
 import '../../assets/css/elements/common.css';
 
 export const MainLayout = ({ children }: { children?: React.ReactNode }): JSX.Element => {
     return (
-        <div className='bg-base bg-full'>
+        <div className='bg-base bg-full d-flex'>
+            <Navbar />
             <div className='container'>
                 {children}
             </div>

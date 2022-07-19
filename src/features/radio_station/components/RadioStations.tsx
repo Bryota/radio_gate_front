861db0457a65a60140286ff2de5a8832a6085cf4
sgw-ryota <ryota-segawe@sekawamukufutoshinoMacBook-Pro.local>
2022-07-19 23:35:29 +0900

    ラジオ局絞り込みフォームのデザイン修正

--- src/features/radio_station/components/RadioStations.tsx
+++ src/features/radio_station/components/RadioStations.tsx
@@ -14,7 +14,12 @@ import '../../../assets/css/components/pagination.css';
 
 export const RadioStations = () => {
     const [currentPage, setCurrentPage] = useState<number>(1);
+    const [type, setType] = useState<string>('');
+    const [keyword, setKeyword] = useState<string>('');
     const { apiData: radioStations, isLoading } = useFetchApiData<RadioStationsResponseType>(`${process.env.REACT_APP_RADIO_GATE_API_URL}/api/radio-stations?page=${currentPage}`, currentPage);
+
+    const searchKeyword = async () => {
+    }
 
     const prevPagination = () => {
         setCurrentPage((pre_current_page) => pre_current_page - 1);
@@ -32,6 +37,26 @@ export const RadioStations = () => {
                     title="Radio Station"
                     subtitle='ラジオ局一覧'
                 />
+                <div>
+                    <div className='border-0 text-start p-0 m-0'>
+                        <input id="type-all" type="radio" name="type" value="FM" defaultChecked={true} />
+                        <label className="radio-inline__label" htmlFor="type-all">
+                            ALL
+                        </label>
+                        <input id="type-am" type="radio" name="type" value="AM" />
+                        <label className="radio-inline__label" htmlFor="type-am">
+                            AM
+                        </label>
+                        <input id="type-fm" type="radio" name="type" value="FM" />
+                        <label className="radio-inline__label" htmlFor="type-fm">
+                            FM
+                        </label>
+                    </div>
+                    <div className='mt-4'>
+                        <input type="search" name="keyword" id="keyword" className='search-keyword__input' value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+                        <button className='search-keyword__button' onClick={searchKeyword}>検索</button>
+                    </div>
+                </div>
                 <div>
                     {
                         radioStations?.radio_stations.data.map((RadioStation) => {

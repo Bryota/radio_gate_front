9b386ba87ba9aa18288f5ede5e14030ced757783
sgw-ryota <ryota-segawe@sekawamshinoMBP.lan>
2022-08-09 22:55:15 +0900

    プロフィール画面のレスポンシブデザイン修正

--- src/features/listener/components/ProfileItem.tsx
+++ src/features/listener/components/ProfileItem.tsx
@@ -6,10 +6,10 @@ export const ProfileItem = ({ label, value, is_first_item = false, no_value = fa
     return (
         <>
             <div className={is_first_item ? 'row mt-0 font-25 underline-green' : 'row mt-5 font-25 underline-green'}>
-                <div className='col-4'>
+                <div className='col-12 col-md-4'>
                     {label}
                 </div>
-                <div className={no_value ? 'col-8 color-accent' : 'col-8'}>
+                <div className={no_value ? 'col-12 col-md-8 color-accent' : 'col-8'}>
                     {value}
                 </div>
             </div>
